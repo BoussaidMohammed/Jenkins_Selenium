519aee9d2b2b7de86668395a75562d585491ae32
Mohammed Boussaid <mboussaid@smb.sqli.com>
2024-02-21 10:09:11 +0100

    fixing report naming

--- src/test/java/com/sqli/test_auto/runners/LoginRunner.java
+++ src/test/java/com/sqli/test_auto/runners/LoginRunner.java
@@ -3,7 +3,7 @@ package com.sqli.test_auto.runners;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/com/sqli/test_auto/features/login.feature", glue = "com.sqli.test_auto.steps",
-        monochrome = true, plugin = {"json:target/cucumber-reports/cucumber.json", "html:target/cucumber-reports/cucumber.html"})
+        monochrome = true, plugin = {"json:target/cucumber-reports/login-report.json", "html:target/cucumber-reports/cucumber.html"})
 public class LoginRunner extends TestNGTestRunner {
 
 }
